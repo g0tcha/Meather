593945f6f206e169af435bc88fbcec1ef4fe12cc
Vincent Grossier <vincent.grossier@gmail.com>
2023-05-06 12:48:42 +0200

    Initial Commit

---
+++ Meather/MeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeatherApp.swift
+//  Meather
+//
+//  Created by Vincent Grossier on 06/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
